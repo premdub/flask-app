b98f2c8258f8ffdb1718d13848556e53bfb3a8bd
premdub <111820562+premdub@users.noreply.github.com>
2022-09-05 18:57:59 -0400

    update

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def HHA504():
     return 'This is CLASS HHA504 PAGE!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
