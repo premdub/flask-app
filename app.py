b53904ae06a0b938e0893435ca7ee359b1b9d3cc
premdub <111820562+premdub@users.noreply.github.com>
2022-09-05 22:53:36 -0400

    update

--- app.py
+++ app.py
@@ -4,29 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_priya():
-    return 'HELLO PRIYA!....I HOPE YOU ARE DOING WELLLLL....
+    return "HELLO PRIYA!....I HOPE YOU ARE DOING WELLLLL...."
 
 
 @app.route('/careers')
 def careers():
-	POPULAR CAREERS = {
-		"1" : 	"Dentist",	
-		"2"	:	"Registered Nurse",	
-		"3"	:	"Pharmacist",	
-		"4"	:	"Computer Systems Analyst",	
-		"5"	:	"Physician",	
-		"6"	:	"Database Administrator",
-		"7"	:	"Software Developer",	
-		"8"	:	"Physical Therapist",	
-		"9"	:	"Web Developer",	
-		"10":	"Dental Hygienist",	
-		"11":	"Occupational Therapist",
-		"12":	"Veterinarian",	
-		"13":	"Computer Programmer",	
-		"14":	"School Psychologist",	
-	}
-    return POPULAR CAREERS
-
+    return {"POPULAR CAREERS: 	 1	Dentist	
+								2	Registered Nurse
+       						    3	Pharmacist	
+								4	Computer Systems Analyst	
+								5	Physician"}
+							
 @app.route('/HHA504')
 def HHA504():
     return 'This is CLASS HHA504 PAGE!'
